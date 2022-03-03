9336bdb24572483a84631d3f1865e3c127efe139
rana bahaa <ranabahaa9000@gmail.com>
2022-03-03 13:00:28 +0200

    third commit

---
+++ lib/modules/notification/NotificationScreen.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xffF6F9FA),
+        elevation: 0.0,
+        titleSpacing: 20.0,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_outlined,
+            color: Colors.black,
+          ),
+        ),
+
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  'you have 2 new notifications',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'Today',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.teal,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 4.0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 0.0, top: 5.0),
+                    child: CircleAvatar(
+                      radius: 4.0,
+                      backgroundColor: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                itemBuilder: (BuildContext context, int index)=>BuildChatItem(),
+                separatorBuilder: (BuildContext context, int index)=>SizedBox(
+                  height: 20.0,
+                ),
+                itemCount: 15,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget BuildChatItem()=>Row(
+    children: [
+      Stack(
+        alignment: AlignmentDirectional.bottomEnd,
+        children: [
+          CircleAvatar(
+            radius: 30.0,
+            backgroundImage: NetworkImage('https://static.wikia.nocookie.net/lucerne/images/d/d1/Hermione_Granger2.jpg/revision/latest/scale-to-width-down/1200?cb=20120209185858'),
+          ),
+        ],
+      ),
+      SizedBox(
+        width: 20.0,
+      ),
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  'rana bahaa',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                Container(
+                  width: 50.0,
+                  height: 10.0,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 130.0,top: 0.0,right: 0.0,bottom: 0.0,),
+                    child: MaterialButton(
+                      onPressed: (){},
+                      child: Icon(
+                        Icons.close_rounded,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'transfered 250 LE to you',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
+    ],
+  );
+}
